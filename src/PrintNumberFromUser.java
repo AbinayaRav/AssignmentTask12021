528415bcd13da8dd394ee3cddaf16cfac6336062
AbinayaRav <abirav26@gmail.com>
2021-12-27 16:31:44 -0500

    Latest updates

--- src/PrintNumberFromUser.java
+++ src/PrintNumberFromUser.java
@@ -5,17 +5,17 @@ class TestClass{
 }
 
 public class PrintNumberFromUser {
- private   int number34=12;
+/* private   int number34=12;
 
  public static void helloExample()
  {
      System.out.println(new PrintNumberFromUser().number34);
- }
+ }*/
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //TestClass clas1 = new TestClass();
-        System.out.println(new PrintNumberFromUser().number34);
+       // System.out.println(new PrintNumberFromUser().number34);
         System.out.println("Please enter data: ");
         String userData = scan.nextLine();
         System.out.println("\nUser entered the below data: ");
